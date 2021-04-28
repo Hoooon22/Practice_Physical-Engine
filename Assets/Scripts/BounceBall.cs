0ec7344af579ee72e228c856e19a01159316c47f
Hoooon22 <momo990305@gmail.com>
2021-04-28 21:50:56 +0900

    sync

--- Assets/Scripts/BounceBall.cs
+++ Assets/Scripts/BounceBall.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BounceBall : MonoBehaviour
 {
-    public Transform Target;
+    public Transform Targets;
 
     // Start is called before the first frame update
     void Start()
